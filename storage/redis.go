5ef7daefe81db597db18769308c348bf29406e31
zen <mbp>
2025-02-08 23:27:16 +0800

    redis连接信息通过外部设置

--- storage/redis.go
+++ storage/redis.go
@@ -3,6 +3,9 @@ package storage
 import (
 	"context"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -12,17 +15,26 @@ var RedisClient *redis.Client
 
 // 初始化 Redis 连接
 func InitRedis() error {
+	host := os.Getenv("REDIS_HOST")
+	port := os.Getenv("REDIS_PORT")
+	addr := strings.Join([]string{host, port}, ":")
+	password := os.Getenv("REDIS_PASSWORD")
+	db, err := strconv.Atoi(os.Getenv("REDIS_DB"))
+	if err != nil {
+		db = 0
+	}
+
 	RedisClient = redis.NewClient(&redis.Options{
-		Addr:     "redis:6379", // 使用容器名和端口
-		Password: "123456",
-		DB:       0, // 使用默认数据库 (0)
+		Addr:     addr, // 使用容器名和端口
+		Password: password,
+		DB:       db, // 使用默认数据库 (0)
 	})
 
 	// 使用 context.Background() 创建一个空的 context
 	ctx := context.Background()
 
 	// 测试连接
-	_, err := RedisClient.Ping(ctx).Result()
+	_, err = RedisClient.Ping(ctx).Result()
 	if err != nil {
 		return fmt.Errorf("Redis 连接失败: %w", err)
 	}
